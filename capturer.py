190b332be180b6271f1797ff0cc73da577c68e23
Richard Ye <yerich@gmail.com>
2013-10-31 21:52:58 -0400

    better error messages, fucntion encapsulation

--- capturer.py
+++ capturer.py
@@ -5,6 +5,7 @@ import time
 import win32com.client as comclt
 import win32api
 import sendinput
+import ctypes
 
 def sendkey(keychar):
     if(isinstance(keychar, (int, long))):
@@ -25,62 +26,71 @@ def grabScreenshotData(bbox):
 
 logfile = open("output/capturelog.txt", "a")
 
-window_title = 'league of legends (tm) client'
-#window_title = 'notepad'
-
-toplist, winlist = [], []
-def enum_cb(hwnd, results):
-    winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
-win32gui.EnumWindows(enum_cb, toplist)
-
-client = [(hwnd, title) for hwnd, title in winlist if window_title in title.lower()]
-client = client[0]
-hwnd = client[0]
-
-win32gui.SetForegroundWindow(hwnd)
-
-time.sleep(0.1)
-
-bbox = win32gui.GetWindowRect(hwnd)
-
-history = []
-pausedcounter = 0
-
-logfile.write("============================================================\n")
-
-while True:
-    start = time.clock()
-    data = grabScreenshotData(bbox)
+def client_capture():
+    window_title = 'league of legends (tm) client'
+    #window_title = 'notepad'
     
-    if(not data):
-        print "Could not load screenshot. Waiting 5 seconds to try again..."
-        time.sleep(5)
-        continue
+    toplist, winlist = [], []
+    def enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+    win32gui.EnumWindows(enum_cb, toplist)
     
-    if(data['loading'] == True):
-        continue
+    client = [(hwnd, title) for hwnd, title in winlist if window_title in title.lower()]
+    if(len(client) == 0):
+        MessageBox = ctypes.windll.user32.MessageBoxA
+        MessageBox(None, "Error: League of Legends client window not found. Make sure that a League of Legends replay is playing.", 'Error', 0)
+        return
+        
+    client = client[0]
+    hwnd = client[0]
     
-    if(data['game_finished'] == True):
-        print "Game Finished."
-        break
+    win32gui.SetForegroundWindow(hwnd)
     
-    print str(data['time']) + ": " + str(data['teams'][0]['gold']) + "|" + str(data['teams'][1]['gold'])
-    logfile.write(str(data['time']) + ": " + str(data['teams'][0]['gold']) + "|" + str(data['teams'][1]['gold'])+"\n")
+    time.sleep(0.1)
     
-    if(data['speed'] != 8):
-        sendkey(0x6B)
+    bbox = win32gui.GetWindowRect(hwnd)
     
-    if(data['gold_data_available'] == False):
-        sendkey('x')
+    history = []
+    pausedcounter = 0
     
-    if(len(history) > 5):
-        if('time' in history[-5] and 'time' in data and data['time'] == history[-5]['time']):
-            pausedcounter += 1
-            if(pausedcounter >= 5):
-                sendkey('p')
+    logfile.write("============================================================\n")
+    
+    while True:
+        start = time.clock()
+        data = grabScreenshotData(bbox)
+        
+        if(not data):
+            print "Could not load screenshot. Waiting 5 seconds to try again..."
+            time.sleep(5)
+            continue
+        
+        if(data['loading'] == True):
+            continue
+        
+        if(data['game_finished'] == True):
+            print "Game Finished."
+            break
+        
+        print str(data['time']) + ": " + str(data['teams'][0]['gold']) + "|" + str(data['teams'][1]['gold'])
+        logfile.write(str(data['time']) + ": " + str(data['teams'][0]['gold']) + "|" + str(data['teams'][1]['gold'])+"\n")
+        
+        if(data['speed'] != 8):
+            sendkey(0x6B)
+        
+        if(data['gold_data_available'] == False):
+            sendkey('x')
+        
+        if(len(history) > 5):
+            if('time' in history[-5] and 'time' in data and data['time'] == history[-5]['time']):
+                pausedcounter += 1
+                if(pausedcounter >= 5):
+                    sendkey('p')
+                    pausedcounter = 0
+            else:
                 pausedcounter = 0
-        else:
-            pausedcounter = 0
-    
-    history.append(data)
-    print "Finished in " + str((time.clock() - start)*1000)+"ms"
+        
+        history.append(data)
+        print "Finished in " + str((time.clock() - start)*1000)+"ms"
+        
+if __name__ == "__main__":
+    client_capture()
