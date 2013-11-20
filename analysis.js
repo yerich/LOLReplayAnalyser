78e05cf2533f1eed0a5b7ea59172ef8c48d52b04
Richard Ye <yerich@gmail.com>
2013-11-20 03:51:19 -0500

    Add full scoreboard to analysis page

--- analysis.js
+++ analysis.js
@@ -5,13 +5,72 @@ function convertDataTime(data) {
     return data;
 }
 
+function championIconTag(name) {
+    return "<img src='icons/champion-"+name+".png' />";
+}
+
+function itemIconTag(name) {
+    return "<img src='icons/item-"+name+".png' />";
+}
+
+function summonerSpellIconTag(name) {
+    return "<img src='icons/summoner-"+name+".png' />";
+}
+
+function quickfindLink(summoner) {
+    return "<a href='http://quickfind.kassad.in/profile/na/"+summoner+"'>"+summoner+"</a>";
+}
+
+function printableChampionName(name) {
+    special_map = {};
+    special_map['khazix'] = "Kha'Zix";
+    special_map['kogmaw'] = "Kog'Maw";
+    special_map['chogath'] = "Cho'gath";
+    
+    if(special_map[name]) {
+        return special_map[name];
+    }
+    else {
+        name = name.replace("-", " ");
+        return name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+}
+
 $(document).ready(function() {
     baseurl = "output/sample/";
     
-    $.getJSON(baseurl+"gold.json", function(data) {
+    $.getJSON(baseurl+"data.json", function(data) {
         console.log(data);
         
-        // Create the chart
+        // Populate the scoreboard
+        for(i = 0; i < 2; i++) {
+            for(j = 0; j < 5; j++) {
+                $("#main_scoreboard_champion_"+i+"_"+j).html(championIconTag(data['game']['players'][i][j]['champion']));
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append("<td>"+championIconTag(data['game']['players'][i][j]['champion'])+"</td>");
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append(
+                    "<td><span class='detailed_scoreboard_level'>"+data['game']['players'][i][j]['level']+"</span></td>");
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append(
+                    "<td><span class='detailed_scoreboard_champion_name'>"+
+                    printableChampionName(data['game']['players'][i][j]['champion'])+"</span><br />"+
+                    "<span class='detailed_scoreboard_summoner_name'>"+quickfindLink(data['game']['players'][i][j]['summoner'])+"</span></td>");
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append("<td class='detailed_scoreboard_summoner_spells'></td>");
+                for(k = 0; k < 2; k++) {
+                    $("#detailed_scoreboard_champion_"+i+"_"+j+" .detailed_scoreboard_summoner_spells").append(summonerSpellIconTag(data['game']['players'][i][j]['summoner-spells'][k]));
+                }
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append("<td class='detailed_scoreboard_items'></td>");
+                for(k = 0; k < data['game']['players'][i][j]['items'].length; k++) {
+                    $("#detailed_scoreboard_champion_"+i+"_"+j+" .detailed_scoreboard_items").append(itemIconTag(data['game']['players'][i][j]['items'][k]));
+                }
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append(
+                    "<td><span class='detailed_scoreboard_level'>"+data['game']['players'][i][j]['kda'].join(" / ")+"</span></td>");
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append(
+                    "<td><span class='detailed_scoreboard_level'>"+data['game']['players'][i][j]['minions']+"</span></td>");
+                $("#detailed_scoreboard_champion_"+i+"_"+j).append(
+                    "<td><span class='detailed_scoreboard_level'>"+data['game']['players'][i][j]['total_gold']+"</span></td>");
+            }
+        }
+        
+        // Create the gold chart
         $('#team_gold_chart').highcharts('StockChart', {
             
 
@@ -82,29 +141,32 @@ $(document).ready(function() {
             series : [
                 {
                     name : 'Blue Team',
-                    data : convertDataTime(data['teams'][0]),
+                    data : convertDataTime(data['gold']['teams'][0]),
                     tooltip: {
                         valueDecimals: 0
                     },
-                    lineColor: "#0046AF"
+                    lineColor: "#0046AF",
+                    color: "#0046AF"
                 },
                 {
                     name : 'Purple Team',
-                    data : convertDataTime(data['teams'][1]),
+                    data : convertDataTime(data['gold']['teams'][1]),
                     tooltip: {
                         valueDecimals: 0
                     },
-                    lineColor: "#7000AD"
+                    lineColor: "#7000AD",
+                    color: "#7000AD"
                 },
                 {
                     name : 'Difference',
                     type : 'area',
                     fillColor : "rgba(0, 0, 0, 0.3)",
-                    data : convertDataTime(data['difference']),
+                    data : convertDataTime(data['gold']['difference']),
                     tooltip: {
                         valueDecimals: 0
                     },
                     lineColor: "#000",
+                    color: "#000",
                     yAxis: 1
                 }
             ],
--- analyze.py
+++ analyze.py
@@ -2,6 +2,7 @@ from __future__ import division
 import gzip
 import json
 import os
+import re
 
 class LOLGameData:
     data = {}
@@ -30,8 +31,12 @@ class LOLGameData:
                 for player in range(0, 4):
                     data['players'][team][player] = sorted(data['players'][team][player].items(), key=lambda tup: tup[0])
         
-        print data['teams']
         return data
+    
+    def __removeActivatedItems(self, itemList):
+        for k, v in enumerate(itemList):
+            itemList[k] = re.sub("\-activated.*", "", v)
+        return itemList
     
     def getTotalGoldOverTime(self):
         gold_data = { 'teams' : [{}, {}], 'players' : [[{}, {}, {}, {}, {}], [{}, {}, {}, {}, {}]], 'difference' : {}}
@@ -41,19 +46,59 @@ class LOLGameData:
                 gold_data['teams'][1][i['time']] = i['teams'][1]['gold']
                 gold_data['difference'][i['time']] = i['teams'][0]['gold'] - i['teams'][1]['gold']
                 for team in [0, 1]:
-                    for player in range(0, 4):
+                    for player in range(0, 5):
                         gold_data['players'][team][player][i['time']] = i['players'][team][player]['gold']
         
         return self.__convertDataToTuples(gold_data)
+    
+    # Get final scoreboard, summoner names, etc.
+    def getGameData(self):
+        basename = ".".join(self.filename.split(".")[0:-1])
+        game_data = { 'players' : [[], []] }
+        
+        itemAvaliableIndex = len(self.data['history']) - 1
+        while(self.data['history'][itemAvaliableIndex]['item_data_available'] == False):
+            itemAvaliableIndex -= 1
+        
+        goldAvailableIndex = len(self.data['history']) - 1
+        while(self.data['history'][goldAvailableIndex]['gold_data_available'] == False):
+            goldAvailableIndex -= 1
+        
+        for i in range (0, 10):
+            game_data['players'][i//5].append({"summoner" : self.data['lrf_meta']['players'][i]['summoner'], 
+                                               "champion" : self.data['history'][0]['players'][i//5][i%5]['champion'],
+                                               "summoner-spells" : self.data['summoner_spells'][i//5][i%5],
+                                               "items" : self.__removeActivatedItems(self.data['history'][itemAvaliableIndex]['players'][i//5][i%5]['items']),
+                                               "total_gold" : self.data['history'][goldAvailableIndex]['players'][i//5][i%5]['total_gold'],
+                                               "kills" : self.data['history'][-1]['players'][i//5][i%5]['kills'],
+                                               "deaths" : self.data['history'][-1]['players'][i//5][i%5]['deaths'],
+                                               "assists" : self.data['history'][-1]['players'][i//5][i%5]['assists'],
+                                               "kda" : self.data['history'][-1]['players'][i//5][i%5]['kda'],
+                                               "minions" : self.data['history'][-1]['players'][i//5][i%5]['minions'],
+                                               "level" : self.data['history'][-1]['players'][i//5][i%5]['level']})
+        
+        for team in [0, 1]:
+            for player in range(0, 5):
+                reverse_counter = 1
+                while(game_data['players'][team][player]['level'] == 0):
+                    reverse_counter += 1
+                    game_data['players'][team][player]['level'] = self.data['history'][-reverse_counter]['players'][team][player]['level']
+        
+        json.dump(game_data, open(basename+"/data2.json", "w+"))
+        return game_data
     
     def generateAnalysisFile(self):
         basename = ".".join(self.filename.split(".")[0:-1])
         if not os.path.exists(basename):
             os.makedirs(basename)
         
-        json.dump(self.getTotalGoldOverTime(), open(basename+"/gold.json", "w+"))
+        jsondata = { 
+                    "gold" : self.getTotalGoldOverTime(),
+                    "game" : self.getGameData() 
+                    }
+        json.dump(jsondata, open(basename+"/data.json", "w+"))
 
 if __name__ == "__main__":
-    data = LOLGameData("output/sample.lra")
+    data = LOLGameData("output/Gentium - Sona (7) - Spec.lra")
     print str(len(data.data['history'])) + " data points loaded."
     data.generateAnalysisFile()
